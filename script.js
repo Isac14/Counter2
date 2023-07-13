0f3e6385e11e08d04912fca072317f7ee3b543ff
Isac <isac.mamede14@gmail.com>
2023-07-13 10:49:22 -0300

    redesigned

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const increase = document.querySelector('#btnIncrease')
 const number = document.querySelector('#number')
 const decrease = document.querySelector('#btnDecrease')
+const reset = document.querySelector('#btnReset')
 
 var n = 0
 
@@ -11,5 +12,10 @@ increase.addEventListener('click', function(){
 
 decrease.addEventListener('click', function(){
     n = --n
+    number.innerHTML = n
+})
+
+reset.addEventListener('click', function(){
+    n = 0
     number.innerHTML = n
 })
