d279dd26da56ebdc5ec9b2777822f61d3f1c66c9
Isac <isac.mamede14@gmail.com>
2023-07-13 11:45:13 -0300

    color upgrade

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
+// selecting the elements
 const increase = document.querySelector('#btnIncrease')
 const number = document.querySelector('#number')
 const decrease = document.querySelector('#btnDecrease')
 const reset = document.querySelector('#btnReset')
 
+// set the initial count
 var n = 0
 
+
+
+// functions
 increase.addEventListener('click', function(){
     n = ++n
     number.innerHTML = n
+    color(n)
 })
 
 decrease.addEventListener('click', function(){
     n = --n
     number.innerHTML = n
+    color(n)
 })
 
 reset.addEventListener('click', function(){
     n = 0
     number.innerHTML = n
+    
 })
+
+function color (n){
+    if(n < 0){
+        number.style.color = 'red'
+    }
+    else if (n > 0){
+        number.style.color = 'rgb(11, 238, 15)'
+    }else{
+        number.style.color = 'black'
+    }
+}
